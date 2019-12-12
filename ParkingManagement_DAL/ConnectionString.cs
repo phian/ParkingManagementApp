7776a1c3cd9fcdda81d322b6ad589ed617e5af8b
hoangvinh121299 <42103527+hoangvinh121299@users.noreply.github.com>
2019-12-13 00:40:50 +0700

    13-12-2019 12:39AM
    Reinstall DTO, DAL and BUS
    Create Connectiongstring to Database
    Add name for Database

---
+++ ParkingManagement_DAL/ConnectionString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+namespace ParkingManagement_DAL
+{
+    public class ConnectionString
+    {
+        // Đường dẫn database
+        public static string connectionString = "Data Source=" + Dns.GetHostName() + ";Initial Catalog=PARKINGMANAGE;Integrated Security=True";
+        public SqlConnection GetConnect()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
